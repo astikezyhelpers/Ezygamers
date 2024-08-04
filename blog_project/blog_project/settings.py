a8cdb14c1cf114097fab0bfefdfd53b41b03dee4
astikezyhelpers <astik.ezyhelpers@gmail.com>
2024-08-04 21:12:12 +0530

    Updated Database & style.

--- blog_project/blog_project/settings.py
+++ blog_project/blog_project/settings.py
@@ -91,12 +91,23 @@ WSGI_APPLICATION = 'blog_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ezygamers',
+        'USER': 'admin',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',  # Or the hostname where your PostgreSQL server is running
+        'PORT': '5432',  # Or the port your PostgreSQL server is running on
     }
 }
+
 
 
 # Password validation
